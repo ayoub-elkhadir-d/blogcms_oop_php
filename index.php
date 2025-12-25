78a27c35cded89e7428aa158df3b864513840c81
Ayoub Elkhadir <assiacodi@gmail.com>
2025-12-25 17:26:17 +0100

    Refactor User and Author methods for article handling

--- index.php
+++ index.php
@@ -1,8 +1,32 @@
 <?php
+$users = [
+    new Author(1,"AminaAdmin","amina@mediapress.com","admin_2025",null,new DateTime('2025-01-01 08:00:00'),new DateTime('2025-12-24 10:30:00')),
+    new Author(2,"ThomasEdit","thomas@mediapress.com", "editor_pass",null, new DateTime('2025-02-15 09:15:00'),new DateTime('2025-12-20 16:45:00') ),
+    new Author( 3,"LeaWriter", "lea@mediapress.com","writing_fun",null, new DateTime('2025-03-10 11:00:00'),new DateTime('2025-12-25 09:00:00')),
+    new Author(4, "MarcoDev","marco@external.com","guest_pwd",null,new DateTime('2025-05-20 14:00:00'),new DateTime('2025-12-25 09:00:00'))
+];
+$articles = [
+    new Article(1,"Bienvenue sur BlogCMS","Voici le premier article de notre système en ligne de commande.",5),
+    new Article(2, "Maîtriser la POO PHP","La programmation orientée objet permet de structurer son code proprement.",3 ),
+    new Article(3, "Maintenance Serveur","Une maintenance est prévue ce soir à 22h pour optimiser les performances.",1)
+];
+$categories = [
+    
+    new Category(1, "Technologie", null),
+    new Category(2, "Corporate", null),
+    new Category(3, "Hardware", null),
+    new Category(4, "Programmation", 1),
+    new Category(5, "Réseaux", 1),
+    new Category(6, "PHP", 4),
+    new Category(7, "JavaScript", 4),
+    new Category(8, "Événements", 2)
+];
 
-/*mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-**********************  USER  *************************
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*/
+
+
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*/
+// **********************  USER  *************************
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*/
 class User
 {
     protected int $id;
@@ -23,37 +47,69 @@ class User
         $this->lastLogin = new DateTime();
     }
 
-    public function readArticle(Article $article)
+    public function readArticle($id, $articles)
     {
-        return $article->getContent();
+        foreach($articles as $article){
+            if($article->getId() === $id){
+         return $article->getContent($id, $articles);           
+         }
+        } 
+
+        
     }
 
-    public function writeComment(Article $article, string $content)
+    public function writeComment($id,$array_article, string $content)
     {
-        return new Comment(null, $content, $this->id, $article->getId());
+        foreach($array_article as $article){
+            if($article->getId() === $id){
+                $article->addComment(new Comment(1, $content, $this->id, $article->getId()));
+            }
+
+        }
+     
     }
 
     public function getId()
     {
         return $this->id;
+    } 
+
+    public function  login (array $array_users,$user_,$pass){
+        foreach($array_users as $user){
+              if ($user->username === $user_ && $user->password === $pass) {
+            return true;
+            $is_login = true;
+           }  
+        }
+    }
+      
+public function get_all_articles($articles){
+    foreach($articles as $article){
+        echo $this->readArticle($article->getId(), $articles);
     }
 }
 
-/*mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*
+}
+
+
+
+/*mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*
 **********************  AUTHOR  ***********************
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*/
+mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*/
 class Author extends User
 {
-   
-
-    public function createArticle(string $title, string $content, Category $category)
+    public function createArticle($id,string $title, string $content, Category $category)
     {
-        return new Article(null, $title, $content, $category->getId());
+        return new Article($id, $title, $content, $category->getId());
     }
 
-    public function deleteOwnArticle(Article $article)
+    public function deleteOwnArticle($articles,$id)
     {
-
+         foreach($articles as $article){
+            if($article->getId() === $id){
+             unset($articless[$id]);
+         }
+        } 
     }
 
     public function updateOwnArticle(Article $article, string $title, string $content)
@@ -93,32 +149,47 @@ class Article
        
     }
 
-    // public function removeCategory()
-    // {
-    //     $this->category_id = 0;
-   
-    // }
-
-    public function publish() { }
-    public function unpublish() { }
-    public function archiver(){ }
+    public function publish(){}
+    public function unpublish(){}
+    public function archiver(){}
 
     public function getId()
     {
         return $this->id;
     }
-
-    public function getContent()
+     public function addComment(Comment $comment)
     {
-        return $this->content;
+        $this->comments[] = $comment;
     }
+
+  public function getContent()
+        {
+            $count_cmt = count($this->comments);
+
+            return "============== {$this->id} =================
+        | {$this->title}
+        ===================================
+        | {$this->content}
+        ===================================
+        Comments: {$count_cmt}
+        ===================================\n";
+        }
+
 
     public function setTitle($title) { $this->title = $title; }
     public function setContent($content) { $this->content = $content; }
+    
+     public function get_all_articles($articles){
+            foreach($articles as $art){
+            readArticle($art->$id, $articles);
+            }
+       }
 }
 
+
+
 /*mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*
-********************** CATEGORY ***********************
+********************** CATEGORY **********************
 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*/
 class Category
 {
@@ -133,8 +204,6 @@ class Category
         $this->name = $name;
         $this->parentCategoryId = $parent;
     }
-
-
 
     public function update(array $data)
     {
@@ -176,10 +245,21 @@ class Comment
         
     }
 
-    public function delete()
-    {
+    public function delete(){
         
     }
+    // public function get_all_comments($id,$articles)
+    // {
+    //       foreach($articles as $art){
+    //           if ($art->id === $id ) {
+    //             foreach($art->comments as $cmt){
+    //                echo  $cmt->content;
+    //             }
+    //      }  
+    //     }
+    // }
+
+    
 }
 
 
@@ -188,7 +268,7 @@ class Comment
 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*/
 class Moderation
 {
-    public function createAssignArticle() {}
+    public function createAssignArticle(){}
     public function deleteArticle(Article $article) {}
     public function updateArticle(Article $article) {}
     public function publierArticle(Article $article) {}
@@ -221,5 +301,42 @@ class Admin extends Moderation
     public function deleteUser(User $user) {}
 
 }
+
+
+
+/*===================================================*/
+$admin = new Author(1,"AminaAdmin","amina@mediapress.com","admin_2025",null,new DateTime('2025-01-01 08:00:00'),new DateTime('2025-12-24 10:30:00'));
+ $art= new Article(1,"Bienvenue sur BlogCMS","Voici le premier article de notre système en ligne de commande.",5);
+while(true){
+echo "================== \n 1)display all articles \n 2) write comment \n 3) se connect \n";
+$choix = readline('Enter : ');
+
+switch ($choix) {
+   case 1:
+    $admin->get_all_articles($articles);
+       break;
+   case 2:
+       $choix_id = (int) readline('Enter id de post : ');
+       $content = readline('Enter content de comment : ');
+       $admin -> writeComment($choix_id,$articles,$content);
+       break;
+   case 3:
+       echo "Your favorite color is green!";
+       break;
+
+}
+}
+
+// // echo $admin -> login($users,"AminaAdmin","admin_2025");
+// $admin -> writeComment(1,$articles,"hi");
+
+// array_push($articles,$admin-> createArticle(count($articles)+1,"title","article content4",new Category(8, "Événements", 2)));
+
+// echo $admin ->readArticle(1,$articles);
+
+
+
+
+
 
 ?>
